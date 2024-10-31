56e23dc5fd5ad92525f32353f2f73fc4dce004d6
Rohit Nair <6954311+rnair98@users.noreply.github.com>
2024-10-30 20:33:32 -0400

    chore: migrate to uv

---
+++ Makefile
@@ -0,0 +1,19 @@
+.PHONY: all clean tests
+
+SHELL := /bin/bash
+
+GIT_COMMIT := $(shell git rev-parse HEAD)
+GIT_SHORT := $(shell git rev-parse --short HEAD)
+
+clean:
+	@echo "Cleaning up..."
+	rm -rf build dist *.egg-info
+	find . -type d -name __pycache__ -exec rm -rf {} +
+	find . -type f -name '*.pyc' -delete
+	find . -type f -name '*.pyo' -delete
+	find . -type f -name '*.pyd' -delete
+	find . -type f -name '*.so' -delete
+	find . -type f -name '*.dll' -delete
+	find . -type f -name '*.dylib' -delete
+
+export PYTHONPATH := $(shell pwd)
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
+import csv
+import io
+
 from fasthtml.common import *
+from fastsql import *
+import sqlite3 
+from sqlite_minutils.db import NotFoundError
+from logging import get_logger
 
-app,rt = fast_app()
 
-@rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
+app, route = fast_app()
+logger = get_logger("fasthtml","INFO")
+
+def init_db():
+    conn = sqlite3.connect("todo.db")
+    conn.execute("CREATE TABLE IF NOT EXISTS todo (id INTEGER PRIMARY KEY AUTOINCREMENT, task TEXT NOT NULL, status TEXT NOT NULL)")
+    conn.close()
+
+init_db()
+
+
+
+@route('/')
+def index():
+    return Div(P('Hello World!'), hx_get="/change")
 
 serve()
